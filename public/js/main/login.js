44b431e9e261652f036bd0565fb364ce8edb43e9
steve <a0911558945@gmail.com>
2017-09-13 00:11:33 +0800

    20170913-login與index update

--- public/js/main/login.js
+++ public/js/main/login.js
@@ -1,4 +1,30 @@
+$(document).ready (function(){
+    $('.alert_error').hide();
+})
+
+/**
+ * chk()
+ * 此function為點下submit後先onsubmit進入的function
+ * 若輸入格式不符則直接回傳false，就不會進入後端
+ * @param username 使用者輸入的帳號
+ * @param password 使用者輸入的密碼
+ */
 function chk(){
-    $('#login_message').text("qweqwewqeq");
-    retrun ;
+    var username=$('.username').val();
+    var password=$('.password').val();
+   // console.log(username+password);
+    if(username==''){
+        $('.alert_error').show();
+        $('.alert_error_message').text("帳號不可為空");
+        $('.username').focus();
+        $(".alert_error").fadeTo(1000, 500).slideUp(500);//顯示後滑出
+        return false;        
+    }else if(password==''){
+        $('.alert_error').show();        
+        $('.alert_error_message').text("密碼不可為空");        
+        $('.password').focus();    
+        $(".alert_error").fadeTo(1000, 500).slideUp(500);//顯示後滑出        
+        return false;
+    }
+    return true;
 }
